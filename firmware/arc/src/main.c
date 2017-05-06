f2b97f1e1bce5f47c14cf290c77cddff25092b64
Jack Bradach <jack@bradach.net>
2017-05-06 14:00:24 -0700

    Firmware builds and both cores can send messages to the uart.

--- firmware/arc/src/main.c
+++ firmware/arc/src/main.c
@@ -1,16 +1,24 @@
 
-#include <zephyr/types.h>
-#include <stddef.h>
-#include <string.h>
-#include <errno.h>
+#include <ipm.h>
+#include <ipm/ipm_quark_se.h>
 #include <misc/printk.h>
-#include <misc/byteorder.h>
 #include <zephyr.h>
 
 
+QUARK_SE_IPM_DEFINE(ping_ipm, 0, QUARK_SE_IPM_INBOUND);
+
+void ping_ipm_callback(void *context, u32_t id, volatile void *data)
+{
+	printk("Ping!\n");
+}
+
 void main(void)
 {
+	struct device *ipm;
 
 	printk("ARC alive!\n");
-	
+
+	ipm = device_get_binding("ping_ipm");
+	ipm_register_callback(ipm, ping_ipm_callback, NULL);
+	ipm_set_enabled(ipm, 1);
 }
